866f9bdf2f4268bd0bf7f503e7393e7a042ae46d
Макс Блажин <mblazhin@bk.ru>
2023-06-11 23:26:51 +0300

    refactor form

--- views/site/calculator.php
+++ views/site/calculator.php
@@ -1,42 +1,49 @@
+<?php
 
+use app\models\CalculatorForm;
+use yii\helpers\Html;
+
+$this->title = "Calculator";
+
+$form = \yii\widgets\ActiveForm::begin();
+$model = new CalculatorForm();
+?>
 <div class="container mt-5">
     <div class="d-flex justify-content-center">
-        <form>
-            <fieldset class="form-control" id="disabledInput" type="text" placeholder="Disabled input here...">
-                <legend>Калькулятор расчета стоимости доставки</legend>
-                <div>
-                    <div class="mb-3">
-                        <label for="disabledSelect" class="form-label">Месяц текущего года</label>
-                        <select id="disabledSelect" class="form-select">
-                            <option>Январь</option>
-                            <option>Февраль</option>
-                            <option>Август</option>
-                            <option>Сентябрь</option>
-                            <option>Октябрь</option>
-                            <option>Ноябрь</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="disabledSelect" class="form-label">Тип сырья</label>
-                        <select id="disabledSelect" class="form-select">
-                            <option>Шрот</option>
-                            <option>Жмых</option>
-                            <option>Соя</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="disabledSelect" class="form-label">Тоннаж</label>
-                        <select id="disabledSelect" class="form-select">
-                            <option>25</option>
-                            <option>50</option>
-                            <option>75</option>
-                            <option>100</option>
-                        </select>
-                    </div>
+        <fieldset class="form-control" id="disabledInput" type="text" placeholder="Disabled input here...">
+            <legend>Калькулятор расчета стоимости доставки</legend>
+            <div>
+                <div class="mb-3">
+                    <?= $form->field($model, 'month')->dropDownList([
+                        'Январь' => 'Январь',
+                        'Февраль' => 'Февраль',
+                        'Август' => 'Август',
+                        'Сентябрь' => 'Сентябрь',
+                        'Октябрь' => 'Октябрь',
+                        'Ноябрь' => 'Ноябрь',
+                    ]);
+                    ?>
                 </div>
-
-                <button type="submit" class="btn btn-success ">Рассчитать</button>
-            </fieldset>
-        </form>
+                <div class="mb-3">
+                    <?= $form->field($model, 'raw_type')->dropDownList([
+                        'Шрот' => 'Шрот',
+                        'Жмых' => 'Жмых',
+                        'Соя' => 'Соя',
+                    ]);
+                    ?>
+                </div>
+                <div class="mb-3">
+                    <?= $form->field($model, 'tonnage')->dropDownList([
+                        '25' => '25',
+                        '50' => '50',
+                        '75' => '75',
+                        '100' => '100',
+                    ]);
+                    ?>
+                </div>
+            </div>
+            <?= Html::submitButton($content = "Рассчитать", ["class" => "btn btn-success"]) ?>
+        </fieldset>
     </div>
 </div>
+<?php \yii\widgets\ActiveForm::end() ?>
